3f020bce43f5bf476370d6ef1c33f4544f783cb3
Matěj Holeček <st025840@student.cichnovabrno.cz>
2025-10-13 11:15:47 +0200

    Basic Script (dont touch or it will break)

--- KbdInputTest/script.js
+++ KbdInputTest/script.js
@@ -4,13 +4,17 @@ var Xpos = 50;
 var Ypos = 50;
 var width = 10;
 var stepVal = 1;
+var isShiftOn = false;
 
 function updatePosition() {
     img.style.top = Ypos + "%";
     img.style.left = Xpos + "%";
     img.style.width = width + "%";
 }
-
+function updateStepStatus() {
+    var statusText = isShiftOn ? "Step Value: Fast" : "Step Value: Normal";
+    document.getElementById("stepStatus").innerText = statusText;
+}
 window.addEventListener("keydown", function(event) {
     switch (event.key) {
         case "w": case "W": Ypos += stepVal; break;
@@ -19,7 +23,12 @@ window.addEventListener("keydown", function(event) {
         case "d": case "D": Xpos -= stepVal; break;
         case "q": case "Q": width += stepVal * 2; break;
         case "e": case "E": width -= stepVal * 2; break;
-        case "shift": stepVal = 2; break;
+        case "g": case "G": stepVal = 2; break;
+        case "Shift":
+            isShiftOn = !isShiftOn;
+            stepVal = isShiftOn ? 4 : 1;
+            updateStepStatus();
+            break;
     }
     updatePosition();
 });
