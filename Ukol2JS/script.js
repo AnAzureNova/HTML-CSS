0e59eec5f927b3b533c1ffb0e1917da705c4d279
Matthew <realazurenova@gmail.com>
2025-11-29 13:54:24 +0100

    More function tests

--- Ukol2JS/script.js
+++ Ukol2JS/script.js
@@ -1,5 +1,6 @@
 const label = document.getElementById("label");
 
+//addition
 function ukol1(){
     const num1= Number(prompt("Input 1"));
     const num2= Number(prompt("Input 2"));
@@ -8,7 +9,7 @@ function ukol1(){
     console.log(num1 + num2);
 }
 
-
+//even / odd
 function ukol2(){
     const num = Number(prompt("Input Number"));
     if (num % 2 == 0){
@@ -21,7 +22,7 @@ function ukol2(){
     }
 }
 
-
+//C° to F
 function ukol3(){
     const cs = Number(prompt("Input a temperature in C°"));
     const fh = cs * 9/5 + 32;
@@ -29,7 +30,7 @@ function ukol3(){
     console.log(`${cs}C° = ${fh}F`);
 }
 
-
+//pos / neg or zero
 function ukol4(){
     const num = Number(prompt("Input Number"));
     if (num == 0){
@@ -46,7 +47,7 @@ function ukol4(){
     }
 }
 
-
+//does it contain an a i wonder
 function ukol5(){
     const strg = prompt("Enter any word or sentence");
     if (strg.includes("a")){
@@ -59,7 +60,7 @@ function ukol5(){
     }
 }
 
-
+//something with .includes again
 function ukol6(){
     const strg = prompt("Enter any word or sentence");
     const ammountOfLetters = {};
@@ -74,15 +75,15 @@ function ukol6(){
         }
     }
 
-    for(const letter in ammountOfLetters){
+    for(const letter in ammountOfLetters){ //no clue how i managed to get this to work but it does now
         let currOutput = `${letter}: ${ammountOfLetters[letter]} `;
-        stack += currOutput + "<br>"; 
+        stack += currOutput + "<br>";
         console.log(currOutput);
     }
-    label.innerHTML = `${stack}`;
+    label.innerHTML = stack;
 }
 
-
+//prime number
 function ukol7(){
     const num = Number(prompt("Input Number"));
     let isPrime = false;
@@ -95,7 +96,7 @@ function ukol7(){
         label.innerHTML = "Input number isnt a prime number";
         console.log("Input number isnt a prime number");
     }
-}
+} //fo ukol7
 function isPrimeNum(num){
     const sqrtNum = Math.floor(Math.sqrt(num));
     if (num === 1){
@@ -105,4 +106,43 @@ function isPrimeNum(num){
         if (num % i == 0) return false;
     }
     return true;
+}
+
+//uloha 31 - event listener
+const buttonWithListener = document.getElementById("buttonWithListener1");
+buttonWithListener1.addEventListener("click", greetFunc);
+function greetFunc(){
+    alert("HELLO WORLD!");
+}
+
+//arrow func test
+function ukol32(){
+    mult = (a, b) => a*b;
+    label.innerHTML = mult(5, 2); 
+    //NO more CONSOLE output (for noww)
+}
+
+//TIMEE
+const timeOutput = document.getElementById("timeField");
+function ukol33(){ //primer
+    timeOutput.innerHTML = "Grabbing time";
+    setInterval(clock, 1000);
+}
+function clock(){
+    timeOutput.innerHTML = getTime();
+}
+function getTime(){ //time grab
+    var currTime = new Date().toLocaleTimeString();
+    return currTime;
+}
+
+//change bg color
+function ukol34(){
+    changeBG("gray")
+}
+function ukol34revert(){
+    changeBG("white")
+}
+function changeBG(color){
+    document.body.style.backgroundColor = color;
 }
