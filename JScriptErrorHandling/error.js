f9ceff10dbc31eb90e84ebcbcec511e5ec2baa38
Matěj Holeček <st025840@student.cichnovabrno.cz>
2026-01-12 15:16:50 +0100

    Error handling notes

---
+++ JScriptErrorHandling/error.js
@@ -0,0 +1,18 @@
+/*
+- Structure
+Try{
+...Critical code here....
+}
+Catch(e){
+...Code the launches whenever you throw a new error...
+}
+Finally{
+...Launches either way, its kinda useless...
+}
+- Error object == err.name - err.message - err.stack
+- throw new Error(...);
+- async or await
+- ReferenceError, TypeError, SyntaxError, RangeError, URIError
+- Useful when importing or gathering inputs / data or when exporting (ex databases,...)
+
+*/
