9feab99d7dadfc32f527d205e6914d36a6b73b50
Matěj Holeček <st025840@student.cichnovabrno.cz>
2026-01-29 08:42:43 +0100

    Initial commit

---
+++ PHPFirst/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+    <form action="seznam.php" method="get">
+        Word: <input type="text" name="word"><br>
+        Count: <input type="number" name="count"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
